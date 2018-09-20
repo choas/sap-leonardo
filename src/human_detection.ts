3cc0e61c93d07109931318c4ab3e18005d12055d
Lars Gregori <lars.gregori@sap.com>
2018-09-20 23:44:00 +0200

    add human detection image service

--- src/human_detection.ts
+++ src/human_detection.ts
@@ -7,44 +7,78 @@ import * as assert from 'assert';
 
 export class HumanDetection {
 
-    private _apiKey: string;
-    private _baseUrl: string;
+  private _apiKey: string;
+  private _baseUrl: string;
 
-    constructor(apiKey: string, baseUrl: string = "https://sandbox.api.sap.com") {
-        assert(apiKey, "apiKey is required");
-        this._apiKey = apiKey;
-        this._baseUrl = baseUrl
-    }
+  constructor(apiKey: string, baseUrl: string = "https://sandbox.api.sap.com") {
+    assert(apiKey, "apiKey is required");
+    this._apiKey = apiKey;
+    this._baseUrl = baseUrl
+  }
 
-    humanDetection(files: string): Promise<any> {
+  humanDetection(files: string): Promise<any> {
 
-        return new Promise<any>((resolve, reject) => {
+    return new Promise<any>((resolve, reject) => {
 
-            fs.readFile(files, {}, (err, data) => {
-                if (err) {
-                    console.error('ERROR', err);
-                    reject(err);
-                }
-                var formData = {
-                    file: { value: data, options: files }
-                }
+      fs.readFile(files, {}, (err, data) => {
+        if (err) {
+          console.error('ERROR', err);
+          reject(err);
+        }
+        var formData = {
+          file: { value: data, options: files }
+        }
 
-                var headers = {
-                    Accept: 'application/json',
-                    APIKey: this._apiKey
-                }
+        var headers = {
+          Accept: 'application/json',
+          APIKey: this._apiKey
+        }
 
-                var url = this._baseUrl + "/ml/humandetection/human-detection/";
+        var url = this._baseUrl + "/ml/humandetection/human-detection/";
 
-                request.post({ url: url, formData: formData, headers: headers }, (err, response, body) => {
-                    if (err) {
-                        console.error('ERROR', err);
-                        reject(err);
-                    }
-                    resolve(JSON.parse(body));
-                });
-
-            });
+        request.post({ url: url, formData: formData, headers: headers }, (err, response, body) => {
+          if (err) {
+            console.error('ERROR', err);
+            reject(err);
+          }
+          resolve(JSON.parse(body));
         });
-    }
+
+      });
+    });
+  }
+
+  humanDetectionImage(files: string): Promise<any> {
+
+    return new Promise<any>((resolve, reject) => {
+
+      fs.readFile(files, {}, (err, data) => {
+        if (err) {
+          console.error('ERROR', err);
+          reject(err);
+        }
+        var formData = {
+          file: { value: data, options: files }
+        }
+
+        var headers = {
+          APIKey: this._apiKey
+        }
+
+        var url = this._baseUrl + "/ml/humandetection/human-detection/format:image";
+
+        var imageData = [];
+
+        request
+          .post({ url: url, formData: formData, headers: headers })
+          .on('data', (data) => {
+            imageData.push(data);
+          })
+          .on('end', () => {
+            resolve(Buffer.concat(imageData));
+          })
+
+      });
+    });
+  }
 }
--- src/test/test_human_detection.ts
+++ src/test/test_human_detection.ts
@@ -1,60 +1,79 @@
 "use strict";
 
 import { expect } from 'chai';
+//import * as fs from 'fs';
 import { HumanDetection } from "../index";
 
 describe('human detection', () => {
 
+  var humanDetection = new HumanDetection(process.env.API_KEY);
+
+  describe('four people', () => {
+    it('should predict four people', (done) => {
+      humanDetection.humanDetection("./testdata/man-3365368_640.jpg").then(body => {
+
+        expect(body).to.have.property('detection_boxes').to.be.an('array').have.lengthOf(4);
+        expect(body).to.have.property('detection_classes').to.be.an('array').have.lengthOf(4);
+        expect(body).to.have.property('detection_scores').to.be.an('array').have.lengthOf(4);
+        expect(body).to.have.property('num_detections').to.be.equal(4);
+
+        let expected_results = {
+          detection_boxes:
+            [[0.13377800583839417,
+              0.485458105802536,
+              0.9857560396194458,
+              0.7680479288101196],
+            [0.0999714657664299,
+              0.20566348731517792,
+              0.9941292405128479,
+              0.4770410656929016],
+            [0.009488373063504696,
+              0.009807304479181767,
+              0.9621061682701111,
+              0.2491825520992279],
+            [0.16002099215984344,
+              0.7634855508804321,
+              0.9644148945808411,
+              0.9987258911132812]],
+          detection_classes: ['human', 'human', 'human', 'human'],
+          detection_scores:
+            [0.9962896108627319,
+              0.9920268654823303,
+              0.9907668828964233,
+              0.9902926683425903],
+          num_detections: 4
+        }
+
+        for (var i = 0; i < expected_results.detection_boxes[0].length; i++) {
+          expect(body.detection_boxes[0][i]).to.be.equal(expected_results.detection_boxes[0][i]);
+        }
+        for (var i = 0; i < expected_results.detection_classes.length; i++) {
+          expect(body.detection_classes[i]).to.be.equal(expected_results.detection_classes[i]);
+        }
+        for (var i = 0; i < expected_results.detection_scores.length; i++) {
+          expect(body.detection_scores[i]).to.be.equal(expected_results.detection_scores[i]);
+        }
+
+      }).then(done, done);
+    });
+  });
+
+  describe('human detection image', () => {
+
     var humanDetection = new HumanDetection(process.env.API_KEY);
 
     describe('four people', () => {
-        it('should predict four people', (done) => {
-            humanDetection.humanDetection("./testdata/man-3365368_640.jpg").then(body => {
+      it('should predict four people', (done) => {
+        humanDetection.humanDetectionImage("./testdata/man-3365368_640.jpg").then(body => {
 
-                expect(body).to.have.property('detection_boxes').to.be.an('array').have.lengthOf(4);
-                expect(body).to.have.property('detection_classes').to.be.an('array').have.lengthOf(4);
-                expect(body).to.have.property('detection_scores').to.be.an('array').have.lengthOf(4);
-                expect(body).to.have.property('num_detections').to.be.equal(4);
+          expect(body.length).to.be.equal(393486)
 
-                let expected_results = {
-                    detection_boxes:
-                        [[0.13377800583839417,
-                            0.485458105802536,
-                            0.9857560396194458,
-                            0.7680479288101196],
-                        [0.0999714657664299,
-                            0.20566348731517792,
-                            0.9941292405128479,
-                            0.4770410656929016],
-                        [0.009488373063504696,
-                            0.009807304479181767,
-                            0.9621061682701111,
-                            0.2491825520992279],
-                        [0.16002099215984344,
-                            0.7634855508804321,
-                            0.9644148945808411,
-                            0.9987258911132812]],
-                    detection_classes: ['human', 'human', 'human', 'human'],
-                    detection_scores:
-                        [0.9962896108627319,
-                            0.9920268654823303,
-                            0.9907668828964233,
-                            0.9902926683425903],
-                    num_detections: 4
-                }
+          //fs.writeFileSync("human-detection-image.png", body);
 
-                for (var i = 0; i < expected_results.detection_boxes[0].length; i++) {
-                    expect(body.detection_boxes[0][i]).to.be.equal(expected_results.detection_boxes[0][i]);
-                }
-                for (var i = 0; i < expected_results.detection_classes.length; i++) {
-                    expect(body.detection_classes[i]).to.be.equal(expected_results.detection_classes[i]);
-                }
-                for (var i = 0; i < expected_results.detection_scores.length; i++) {
-                    expect(body.detection_scores[i]).to.be.equal(expected_results.detection_scores[i]);
-                }
-
-            }).then(done, done);
-        });
+        }).then(done, done);
+      });
     });
+
+  });
 
 });
