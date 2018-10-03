8b691f5d01f72ca30bb2cdc2a38d58723c14371d
Lars Gregori <lars.gregori@sap.com>
2018-10-03 18:54:25 +0200

    adjust path

--- examples/imageclassification.js
+++ examples/imageclassification.js
@@ -3,7 +3,7 @@ const leonardo = require('sap-leonardo');
 // set the API_KEY in the console, e.g. export API_KEY=abc123
 var imageclassification = new leonardo.Imageclassification(process.env.API_KEY);
 
-imageclassification.classification("./elephant-114543_640.jpg")
+imageclassification.classification("./testdata/elephant-114543_640.jpg")
   .then(body => {
     console.log(JSON.stringify(body, null, "  "));
     var firstResult = body.predictions[0].results[0];
