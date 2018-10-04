9df64034fafbc9f2a1804a34d52b394dd0905d57
Lars Gregori <lars.gregori@sap.com>
2018-10-05 01:06:29 +0200

    tslint ITranslationRequest spanish text changed

--- src/test/test_translation.ts
+++ src/test/test_translation.ts
@@ -1,148 +1,148 @@
 "use strict";
 
-import { expect } from 'chai';
-import { Translation, TranslationRequest } from "../index";
+import { expect } from "chai";
+import { ITranslationRequest, Translation } from "../index";
 
-describe('translation', () => {
+describe("translation", () => {
 
-  var translation = new Translation(process.env.API_KEY);
+  const translation = new Translation(process.env.API_KEY);
 
-  describe('translate text', () => {
+  describe("translate text", () => {
 
-    const text_english = "This service translates text from a source language into several target languages."
-    const text_deutsch = "Dieser Service übersetzt Text aus einer Quellsprache in mehrere Zielsprachen."
-    const text_español = "Este servicio convierte texto de un idioma fuente en varios idiomas de destino."
+    const textEnglish = "This service translates text from a source language into several target languages.";
+    const textDeutsch = "Dieser Service übersetzt Text aus einer Quellsprache in mehrere Zielsprachen.";
+    const textEspañol = "Este servicio convierte texto de un idioma fuente en varios idiomas de destino.";
 
-    it('should translate English to German', (done) => {
+    it("should translate English to German", (done) => {
 
-      let request: TranslationRequest = {
+      const request: ITranslationRequest = {
         sourceLanguage: "en",
         targetLanguages: ["de"],
         units: [
           {
-            value: text_english,
-            key: "TEXT_ENGLISH"
-          }
-        ]
-      }
+            key: "TEXT_ENGLISH",
+            value: textEnglish,
+          },
+        ],
+      };
 
-      translation.translation(request).then(body => {
+      translation.translation(request).then((body) => {
 
-        expect(body).to.have.property('units').to.be.an('array').have.lengthOf(1);
+        expect(body).to.have.property("units").to.be.an("array").have.lengthOf(1);
 
-        expect(body.units[0]).to.have.property('value').to.be.equal(text_english);
+        expect(body.units[0]).to.have.property("value").to.be.equal(textEnglish);
 
-        expect(body.units[0]).to.have.property('translations').to.be.an('array').have.lengthOf(1);
-        expect(body.units[0].translations[0]).to.have.property('language').to.be.equal('de');
-        expect(body.units[0].translations[0]).to.have.property('value').to.be.equal(text_deutsch);
+        expect(body.units[0]).to.have.property("translations").to.be.an("array").have.lengthOf(1);
+        expect(body.units[0].translations[0]).to.have.property("language").to.be.equal("de");
+        expect(body.units[0].translations[0]).to.have.property("value").to.be.equal(textDeutsch);
       }).then(done, done);
     });
 
+    it("should translate German to English", (done) => {
 
-    it('should translate German to English', (done) => {
-
-      let body: TranslationRequest = {
+      const translationRequest: ITranslationRequest = {
         sourceLanguage: "de",
         targetLanguages: ["en"],
         units: [
           {
-            value: text_deutsch,
-            key: "TEXT_DEUTSCH"
-          }
-        ]
-      }
+            key: "TEXT_DEUTSCH",
+            value: textDeutsch,
+          },
+        ],
+      };
 
-      translation.translation(body).then(body => {
+      translation.translation(translationRequest).then((body) => {
 
-        expect(body).to.have.property('units').to.be.an('array').have.lengthOf(1);
+        expect(body).to.have.property("units").to.be.an("array").have.lengthOf(1);
 
-        expect(body.units[0]).to.have.property('value').to.be.equal(text_deutsch);
+        expect(body.units[0]).to.have.property("value").to.be.equal(textDeutsch);
 
-        expect(body.units[0]).to.have.property('translations').to.be.an('array').have.lengthOf(1);
-        expect(body.units[0].translations[0]).to.have.property('language').to.be.equal('en');
-        expect(body.units[0].translations[0]).to.have.property('value').to.be.equal(text_english);
+        expect(body.units[0]).to.have.property("translations").to.be.an("array").have.lengthOf(1);
+        expect(body.units[0].translations[0]).to.have.property("language").to.be.equal("en");
+        expect(body.units[0].translations[0]).to.have.property("value").to.be.equal(textEnglish);
       }).then(done, done);
     });
 
+    it("should translate English to German and Spanish", (done) => {
 
-    it('should translate English to German and Spanish', (done) => {
-
-      let body: TranslationRequest = {
+      const translationRequest: ITranslationRequest = {
         sourceLanguage: "en",
         targetLanguages: ["de", "es"],
         units: [
           {
-            value: text_english,
-            key: "TEXT_ENGLISH"
-          }
-        ]
-      }
+            key: "TEXT_ENGLISH",
+            value: textEnglish,
+          },
+        ],
+      };
 
-      translation.translation(body).then(body => {
+      translation.translation(translationRequest).then((body) => {
 
-        expect(body).to.have.property('units').to.be.an('array').have.lengthOf(1);
+        expect(body).to.have.property("units").to.be.an("array").have.lengthOf(1);
 
-        expect(body.units[0]).to.have.property('value').to.be.equal(text_english);
+        expect(body.units[0]).to.have.property("value").to.be.equal(textEnglish);
 
-        expect(body.units[0]).to.have.property('translations').to.be.an('array').have.lengthOf(2);
-        expect(body.units[0].translations[0]).to.have.property('language').to.be.equal('de');
-        expect(body.units[0].translations[0]).to.have.property('value').to.be.equal(text_deutsch);
-        expect(body.units[0].translations[1]).to.have.property('language').to.be.equal('es');
-        expect(body.units[0].translations[1]).to.have.property('value').to.be.equal(text_español);
+        expect(body.units[0]).to.have.property("translations").to.be.an("array").have.lengthOf(2);
+        expect(body.units[0].translations[0]).to.have.property("language").to.be.equal("de");
+        expect(body.units[0].translations[0]).to.have.property("value").to.be.equal(textDeutsch);
+        expect(body.units[0].translations[1]).to.have.property("language").to.be.equal("es");
+        expect(body.units[0].translations[1]).to.have.property("value").to.be.equal(textEspañol);
       }).then(done, done);
     });
 
-    it('should translate Spanish to English, but Spanish to German is not available', (done) => {
+    it("should translate Spanish to English, but Spanish to German is not available", (done) => {
 
-      let body: TranslationRequest = {
+      const translationRequest: ITranslationRequest = {
         sourceLanguage: "es",
         targetLanguages: ["en", "de"],
         units: [
           {
-            value: text_español,
-            key: "TEXT_ESPAÑOL"
-          }
-        ]
-      }
+            key: "TEXT_ESPAÑOL",
+            value: textEspañol,
+          },
+        ],
+      };
 
-      translation.translation(body).then(body => {
+      translation.translation(translationRequest).then((body) => {
 
-        expect(body).to.have.property('units').to.be.an('array').have.lengthOf(1);
+        expect(body).to.have.property("units").to.be.an("array").have.lengthOf(1);
 
-        expect(body.units[0]).to.have.property('value').to.be.equal(text_español);
+        expect(body.units[0]).to.have.property("value").to.be.equal(textEspañol);
 
-        expect(body.units[0]).to.have.property('translations').to.be.an('array').have.lengthOf(2);
-        expect(body.units[0].translations[0]).to.have.property('language').to.be.equal('en');
-        expect(body.units[0].translations[0]).to.have.property('value').to.be.not.null;
-        expect(body.units[0].translations[1]).to.have.property('language').to.be.equal('de');
-        expect(body.units[0].translations[1]).to.have.property('value').to.be.null;
+        expect(body.units[0]).to.have.property("translations").to.be.an("array").have.lengthOf(2);
+        expect(body.units[0].translations[0]).to.have.property("language").to.be.equal("en");
+        // tslint:disable-next-line:no-unused-expression
+        expect(body.units[0].translations[0]).to.have.property("value").to.be.not.null;
+        expect(body.units[0].translations[1]).to.have.property("language").to.be.equal("de");
+        // tslint:disable-next-line:no-unused-expression
+        expect(body.units[0].translations[1]).to.have.property("value").to.be.null;
       }).then(done, done);
     });
   });
 
-  describe('error coverage', () => {
+  describe("error coverage", () => {
 
-    var translation = new Translation(
+    const translationErr = new Translation(
       process.env.API_KEY,
-      'http://localhost:11111');
+      "http://localhost:11111");
 
-    it('should return connection refused error', (done) => {
+    it("should return connection refused error", (done) => {
 
-      let body: TranslationRequest = {
+      const body: ITranslationRequest = {
         sourceLanguage: "es",
         targetLanguages: ["en", "de"],
         units: [
           {
+            key: "TEXT_ESPAÑOL",
             value: "",
-            key: "TEXT_ESPAÑOL"
-          }
-        ]
-      }
-      translation.translation(body).then(
-        body => { },
-        err => {
-          expect(err).to.have.property('errno').to.be.equal('ECONNREFUSED');
-          expect(err).to.have.property('code').to.be.equal('ECONNREFUSED');
+          },
+        ],
+      };
+      translationErr.translation(body).then(
+        () => { expect.fail(); },
+        (err) => {
+          expect(err).to.have.property("errno").to.be.equal("ECONNREFUSED");
+          expect(err).to.have.property("code").to.be.equal("ECONNREFUSED");
         }).then(done, done);
     });
 
