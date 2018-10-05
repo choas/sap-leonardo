e6daf0c9217a93841b76e69974efb7a1cd6dd78b
Lars Gregori <lars.gregori@sap.com>
2018-10-05 08:13:40 +0200

    change service call order (as described in the blog post)

--- examples/which_shoe.js
+++ examples/which_shoe.js
@@ -7,43 +7,42 @@ const IMG_SHOE2 = 'hiking-shoes-3074971_640.png';
 const IMG_SHOE_TEST = 'converse-2069209_640.jpg';
 
 // set the API_KEY in the console, e.g. export API_KEY=abc123
-var imageFeatureExtraction = new leonardo.ImageFeatureExtraction(process.env.API_KEY);
-
-imageFeatureExtraction.featureExtraction(IMG_PATH + IMG_SHOE1)
+var multiInstanceImageSegmentation = new leonardo.MultiInstanceImageSegmentation(process.env.API_KEY);
+multiInstanceImageSegmentation.instanceSegmentor(IMG_PATH + IMG_SHOE_TEST)
   .then(body => {
-    var vector1 = body.predictions[0].featureVectors;
 
-    var imageFeatureExtraction = new leonardo.ImageFeatureExtraction(process.env.API_KEY);
-    imageFeatureExtraction.featureExtraction(IMG_PATH + IMG_SHOE2)
-      .then(body => {
-        var vector2 = body.predictions[0].featureVectors;
+    var bbox = body.predictions[0].results[0].bbox;
+    var width = bbox.x2 - bbox.x1, height = bbox.y2 - bbox.y1;
 
-        var multiInstanceImageSegmentation = new leonardo.MultiInstanceImageSegmentation(process.env.API_KEY);
-        multiInstanceImageSegmentation.instanceSegmentor(IMG_PATH + IMG_SHOE_TEST)
+    gm(IMG_PATH + IMG_SHOE_TEST)
+      .crop(width, height, bbox.x1, bbox.y1)
+      .write('croped.jpg', function (err) {
+        if (err) {
+          throw "something went wrong", err
+        }
+
+        var imageFeatureExtraction = new leonardo.ImageFeatureExtraction(process.env.API_KEY);
+        imageFeatureExtraction.featureExtraction("croped.jpg")
           .then(body => {
 
-            var bbox = body.predictions[0].results[0].bbox;
-            var width = bbox.x2 - bbox.x1, height = bbox.y2 - bbox.y1;
+            var vectorWhich = body.predictions[0].featureVectors;
+            var imageFeatureExtraction = new leonardo.ImageFeatureExtraction(process.env.API_KEY);
 
-            gm(IMG_PATH + IMG_SHOE_TEST)
-              .crop(width, height, bbox.x1, bbox.y1)
-              .write('croped.jpg', function (err) {
-                if (err) {
-                  throw "something went wrong", err
-                }
+            imageFeatureExtraction.featureExtraction(IMG_PATH + IMG_SHOE1)
+              .then(body => {
+                var vector1 = body.predictions[0].featureVectors;
 
                 var imageFeatureExtraction = new leonardo.ImageFeatureExtraction(process.env.API_KEY);
-                imageFeatureExtraction.featureExtraction("croped.jpg")
+                imageFeatureExtraction.featureExtraction(IMG_PATH + IMG_SHOE2)
                   .then(body => {
-
-                    var vv = body.predictions[0].featureVectors;
+                    var vector2 = body.predictions[0].featureVectors;
 
                     var data =
                     {
                       "0": [
                         {
                           "id": "which_shoe",
-                          "vector": vv
+                          "vector": vectorWhich
                         },
                         {
                           "id": "shoe_1",
@@ -64,7 +63,7 @@ imageFeatureExtraction.featureExtraction(IMG_PATH + IMG_SHOE1)
                         var firstPrediction = body.predictions[0];
                         var firstSimilarVector = firstPrediction.similarVectors[0];
                         var secondSimilarVector = firstPrediction.similarVectors[1];
-                        
+
                         console.log("RESULT:", firstPrediction.id + ':',
                           firstSimilarVector.id + '=' + firstSimilarVector.score,
                           secondSimilarVector.id + '=' + secondSimilarVector.score);
