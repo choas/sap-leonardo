52416b0cb710dfd64f78ba3d69486c9520d2aa31
Lars Gregori <lars.gregori@sap.com>
2018-10-05 22:59:37 +0200

    eslint

--- examples/image_feature_extraction.js
+++ examples/image_feature_extraction.js
@@ -1,12 +1,12 @@
+"use strict";
+
 const leonardo = require('sap-leonardo');
 
 // set the API_KEY in the console, e.g. export API_KEY=abc123
 var imageFeatureExtraction = new leonardo.ImageFeatureExtraction(process.env.API_KEY);
-
 imageFeatureExtraction.featureExtraction("./testdata/chucks-153310_640.png")
-
-  .then(body => {
-    console.log("RESULT:", body.predictions[0].featureVectors.length, 'vectors')
-    // RESULT: 2048 vectors
+  .then((body) => {
+    console.log("result:", body.predictions[0].featureVectors.length, 'vectors');
+    // result: 2048 vectors
   })
-  .catch(err => { console.error(err) });
+  .catch((err) => { console.error(err); });
--- examples/imageclassification.js
+++ examples/imageclassification.js
@@ -1,13 +1,13 @@
+"use strict";
+
 const leonardo = require('sap-leonardo');
 
 // set the API_KEY in the console, e.g. export API_KEY=abc123
 var imageclassification = new leonardo.Imageclassification(process.env.API_KEY);
-
 imageclassification.classification("./testdata/elephant-114543_640.jpg")
-  .then(body => {
-    console.log(JSON.stringify(body, null, "  "));
+  .then((body) => {
     var firstResult = body.predictions[0].results[0];
-    console.log("RESULT:", firstResult.label, firstResult.score)
-    // RESULT: tusker 0.7052137851715088
+    console.log("result:", firstResult.label, firstResult.score);
+    // result: tusker 0.7052137851715088
   })
-  .catch(err => { console.error(err) });
+  .catch((err) => { console.error(err); });
